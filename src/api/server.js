710d982650532352422b93b3ea9d0a9453fed55c
ovodov_ii <pain3007@mail.ru>
2021-01-12 15:47:18 +0300

    feat(src): add api files

---
+++ src/api/server.js
@@ -0,0 +1,73 @@
+
+import { Factory, Model, RestSerializer, Server } from "miragejs";
+import { DESCRIPTIONS, TITLES } from "./const";
+import { generateName, generateDate, generateRandomDescription } from "./utils";
+
+const IdSerializer = RestSerializer.extend({
+  serializeIds: 'always',
+});
+
+new Server({
+  routes() {
+    this.namespace = 'fakeApi';
+
+    this.resource("documents");
+
+    this.post('/search', function (schema, req) {
+      const body = JSON.parse(req.requestBody);
+
+      if (body.id) {
+        return schema.documents.where(doc => doc.id === body.id);
+      } else if (body.creationDate && body.name) {
+        return schema.documents.where(doc => doc.creationDate === body.creationDate && doc.name.toLowerCase().includes(body.name.toLowerCase()));
+      } else if (body.creationDate) {
+        return schema.documents.where(doc => doc.creationDate === body.creationDate);
+      } else if (body.name) {
+        return schema.documents.where(doc => doc.name.toLowerCase().includes(body.name.toLowerCase()));
+      }
+
+      return schema.documents.none();
+    })
+  },
+  models: {
+    document: Model
+  },
+  factories: {
+    document: Factory.extend({
+      id(i) {
+        return Number(i)
+      },
+      name() {
+        return generateName(TITLES)
+      },
+      creationDate() {
+        return generateDate()
+      },
+      description() {
+        return generateRandomDescription(DESCRIPTIONS)
+      }
+    }),
+  },
+  serializers: {
+    document: IdSerializer.extend({
+      serialize(object, request) {
+        // HACK Mirage keeps wanting to store integer IDs as strings. Always convert them to numbers for now.
+        const numerifyId = (doc) => {
+          doc.id = Number(doc.id)
+        }
+        let json = IdSerializer.prototype.serialize.apply(this, arguments)
+
+        if (json.document) {
+          numerifyId(json.document)
+        } else if (json.documents) {
+          json.documents.forEach(numerifyId)
+        }
+
+        return json
+      },
+    }),
+  },
+  seeds(server) {
+    server.createList('document', 10000)
+  }
+});
---
+++ src/api/utils.js
@@ -0,0 +1,56 @@
+const formatDate = (date) => {
+    let dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+
+    let mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+
+    let yy = date.getFullYear();
+
+    return `${yy}-${mm}-${dd}`;
+};
+
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const randomFromArray = (array) => {
+    const index = getRandomInt(0, array.length - 1);
+    return array[index];
+};
+
+export const generateName = (template) => {
+    const adj1 = Math.floor(Math.random() * template[0].length);
+    const adj2 = Math.floor(Math.random() * template[1].length);
+    const adj3 = Math.floor(Math.random() * template[2].length);
+    const adjective1 = template[0][adj1];
+    const adjective2 = template[1][adj2];
+    const adjective3 = template[2][adj3];
+
+    return `${adjective1} ${adjective2} ${adjective3}`;
+};
+
+export const generateRandomDescription = (template) => {
+    const result = [];
+    for (let i = 0; i < getRandomInt(1, 7); i++) {
+        result.push(randomFromArray(template));
+    }
+
+    return result.join(",");
+};
+
+export const generateDate = () => {
+    const maxDaysGap = getRandomInt(1, 365);
+    const daysGap = getRandomInt(-maxDaysGap, maxDaysGap);
+    let currentDate = new Date();
+
+    currentDate.setHours(23, 59, 59, 999);
+
+    currentDate.setDate(currentDate.getDate() + daysGap);
+
+    currentDate = new Date(currentDate);
+
+    return formatDate(currentDate);
+};
